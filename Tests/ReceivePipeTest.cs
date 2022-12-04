3ddc78710c294ef15dcbcc9e9aa8254d63d09f11
Berke Kaan Çetinkaya <46684966+kaancetinkayasf@users.noreply.github.com>
2022-12-04 20:20:09 +0300

    Added test for peeking

--- Tests/ReceivePipeTest.cs
+++ Tests/ReceivePipeTest.cs
@@ -14,16 +14,30 @@ namespace Tests
         public void TestEnqueue()
         {
             pipe = new ReceivePipe(64);
-            Assert.AreEqual(pipe.MessageQueueCount, 0);
+            Assert.AreEqual(pipe.MessageQueueCount(), 0);
 
             ArraySegment<byte> segment = Encoding.ASCII.GetBytes("Hello");
             Entry entry = new Entry(0,segment, EventType.Data);
 
             pipe.Enqueue(entry);
 
-            Assert.AreEqual(pipe.MessageQueueCount, 1);
+            Assert.AreEqual(pipe.MessageQueueCount(), 1);
 
             Assert.AreEqual(pipe.MessageCountForThisClient(entry.connectionId), 1);
+            
+        }
+
+        [TestMethod]
+        public void EnqueueThenDequeue()
+        {
+            pipe = new ReceivePipe(64);
+            ArraySegment<byte> segment = Encoding.UTF8.GetBytes("Hello");
+            Entry entry = new Entry(0, segment, EventType.Data);
+
+            pipe.Enqueue(entry);
+
+            Assert.IsTrue(pipe.Peek(out Entry e));
+            Assert.AreEqual("Hello",Encoding.UTF8.GetString(e.data));
             
         }
     }
