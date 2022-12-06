0b88eeff60bd8ca0782b1b9d5daf51fc1a42ca32
Berke Kaan Çetinkaya <46684966+kaancetinkayasf@users.noreply.github.com>
2022-12-06 15:22:08 +0300

    initial commit

---
+++ Dives/SendPipe.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dives
+{
+    public class SendPipe
+    {
+        private object locker = new object();
+
+        Queue<ArraySegment<byte>> messageQueue = new Queue<ArraySegment<byte>>();
+
+        Pool<byte[]> pool;
+
+        public SendPipe(int MaxMessageSize)
+        {
+            pool = new Pool<byte[]>(() => new byte[MaxMessageSize]);
+        }
+
+        public void Enqueue(ArraySegment<byte> message)
+        {
+            lock (locker)
+            {
+                ArraySegment<byte> segment = default;
+
+                byte[] bytes = pool.Take();
+
+                Buffer.BlockCopy(message.Array, 0, bytes, 0, message.Count);
+
+                segment = new ArraySegment<byte>(bytes, 0, message.Count);
+
+                messageQueue.Enqueue(segment); 
+            }
+        }
+
+
+        public bool DequeueAndSerializeAll(ref byte[] payload, out int packetSize)
+        {
+            lock (locker)
+            {
+               packetSize = 0;
+               
+                // return immediately if queue is empty.
+                if(messageQueue.Count == 0)
+                {
+                    return false;
+                }
+
+                // 4 byte header + message
+                foreach (ArraySegment<byte> message in messageQueue)
+                {
+                    packetSize += 4 + message.Count;
+                }
+
+                if(payload==null || payload.Length < packetSize)
+                {
+                    payload = new byte[packetSize];
+                }
+
+                int position = 0;
+                while(messageQueue.Count > 0)
+                {
+                    ArraySegment<byte> message = messageQueue.Dequeue();
+
+                    Utils.IntToBytesBigEndianNonAlloc(message.Count, ref payload, position);
+                    position += 4;
+
+                    Buffer.BlockCopy(message.Array, 0, payload, position, message.Count);
+                    position += message.Count;
+
+                    pool.Return(message.Array);
+                }
+
+                return true;
+            }
+        }
+
+        public int GetMessageQueueCount()
+        {
+            lock (locker)
+            {
+                return messageQueue.Count;
+            }
+        }
+
+        
+
+        public int GetPoolCount()
+        {
+            lock (locker)
+            {
+                return pool.Count();
+            }
+        }
+    }
+}
---
+++ Dives/Utils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dives
+{
+    public class Utils
+    {
+
+        // int is 32 bytes. We hold the header information in an integer (int messageSize in the parameters). By shifting the bits to the 24,16,8, times,
+        // we put the leftern most 8 bits to the first element of the byte array. (This is big endian, most significant bits are on the left.)
+        // Let's say messageSize parameter is 12 which is 00000000 00000000 00000000 00001100. Shifting the bits to the right 24 times will put the first 8 bytes at the end.
+        // So bytes in the integer, and the bytes in the byte array are going to be like this at each step: 
+        // 00000000 00000000 00000000 00000000 -  00000000 <-- first element of byte array
+        // 00000000 00000000 00000000 00000000 -  00000000 <-- second element of byte array and so on
+        // 00000000 00000000 00000000 00000000 -  00000000
+        // 00000000 00000000 00000000 00001100 -  00001100
+        // Note: casting 32 bit value to 8 bit value will only copy the rigthern most 8 bit.
+        public static void IntToBytesBigEndianNonAlloc(int messageSize , ref byte[] payload, int position)
+        {
+            payload[position + 0 ] = (byte)(messageSize >> 24);
+            payload[position + 1 ] = (byte)(messageSize >> 16);
+            payload[position + 2 ] = (byte)(messageSize >> 8);
+            payload[position + 3 ] = (byte)(messageSize);
+        }
+    }
+}
